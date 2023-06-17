e0442f6d045fc1983a837099475e5c1f8873ace2
DestinedCodes <yiradesat@gmail.com>
2023-06-17 23:07:11 +0100

    Displays properly

--- matrix.c
+++ matrix.c
@@ -18,18 +18,14 @@ int main(int argc, char *argv[])
         matrix = malloc(sizeof(int *) * size);
 
         if (matrix == NULL)
-        {
                 return (400);
-        }
 
         for (row = 0; row < size; row++)
         {
                 matrix[row] = malloc(sizeof(int) * size);
 
                 if (matrix[row] == NULL)
-                {
                         return (400);
-                }
         }
 
         printf("Enter a %d x %d matrix.\nIn this format: ", size, size);
@@ -37,34 +33,28 @@ int main(int argc, char *argv[])
         for (i = 0; i < size; i++)
         {
                 for(j = 0; j < size; j++)
-                {
                         printf(" %d", rand() % 10);
-                }
-                printf(",");
+
+		if (i < size - 1)
+			printf(",");
         }
         printf("\n");
 
         for (i = 0; i < size; i++)
         {
                 for(j = 0; j < size; j++)
-                {
 			scanf("%d%*c", &matrix[i][j]);
-		}
         }
 
         for (i = 0; i < size; i++)
         {
                 for(j = 0; j < size; j++)
-                {
-                        printf(" %i", matrix[i][j]);
-                }
+                        printf("%i ", matrix[i][j]);
                 printf("\n");
         }
 
         for (row = 0; row < size; row++)
-        {
                 free(matrix[row]);
-        }
 
         free(matrix);
 
